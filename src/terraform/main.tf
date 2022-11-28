04312f4980f77cb1df8924050964d6eb93a5373b
Erick Medeiros <erickb_m@hotmail.com>
2022-11-28 20:04:46 -0300

    Tudo novamente

---
+++ src/terraform/main.tf
@@ -0,0 +1,234 @@
+#Arquivo de provisionamento de recursos no azure ---
+#Estudo de Terraform - Erick Medeiros ---
+#Projeto Staging 01 
+
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.32"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+
+}
+
+#backup do estado 
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "RG-STO-BK"
+    storage_account_name = "stobackupterraform"
+    container_name       = "tfstate"
+    key                  = "staging.terraform.tfstate"
+    #use_azuread_auth     = true
+    #subscription_id      = "2225e25f-3762-422a-85f7-d74547dc8640"
+    #tenant_id            = "ab29f776-7618-47c6-a0f7-94c8cca60f98"
+  }
+}
+
+
+# Deploy de Recursos
+# Deploy Resource Group
+
+resource "azurerm_resource_group" "RG" {
+  name     = "RG-TESTE"
+  location = "eastus"
+  tags = {
+    "env"     = "staging"
+    "project" = "estudo"
+  }
+}
+
+# Deploy Storage Account
+resource "azurerm_storage_account" "sto" {
+  name                     = "stotestezafitec001"
+  resource_group_name      = azurerm_resource_group.RG.name
+  location                 = azurerm_resource_group.RG.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Deploy Azure Files
+resource "azurerm_storage_share" "share" {
+  name                 = "files-prd"
+  storage_account_name = azurerm_storage_account.sto.name
+  quota                = 10
+
+}
+
+# Deploy VNET
+resource "azurerm_virtual_network" "vnet" {
+  name                = "VNET-01"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+  address_space       = ["10.10.0.0/16"]
+}
+
+# Deploy Subnet
+resource "azurerm_subnet" "sub1" {
+  name                 = "SUB-LAN01"
+  resource_group_name  = azurerm_resource_group.RG.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.0.0/24"]
+}
+
+# Deploy Subnet - 02
+resource "azurerm_subnet" "sub2" {
+  name                 = "SUB-LAN02"
+  resource_group_name  = azurerm_resource_group.RG.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
+
+# Deploy NSG1
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "NSG-VMs"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+
+  security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "10.10.0.0/24"
+  }
+}
+
+# Deploy NSG2
+resource "azurerm_network_security_group" "nsg2" {
+  name                = "NSG-DB"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+
+  security_rule {
+    name                       = "Allow-MSSQL"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1433"
+    source_address_prefix      = "*"
+    destination_address_prefix = "10.11.0.0/24"
+  }
+}
+
+# Associar NSG Subnet01
+resource "azurerm_subnet_network_security_group_association" "nsg1" {
+  subnet_id                 = azurerm_subnet.sub1.id
+  network_security_group_id = azurerm_network_security_group.nsg1.id
+}
+
+# Associar NSG Subnet02
+resource "azurerm_subnet_network_security_group_association" "nsg2" {
+  subnet_id                 = azurerm_subnet.sub2.id
+  network_security_group_id = azurerm_network_security_group.nsg2.id
+}
+
+# Deploy Public IP - SRV 01
+resource "azurerm_public_ip" "PIP01" {
+  name                = "PIP-VM-SRV01"
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+  allocation_method   = "Dynamic"
+}
+
+# Deploy Public IP - SRV 02
+resource "azurerm_public_ip" "PIP02" {
+  name                = "PIP-VM-SRV02"
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+  allocation_method   = "Dynamic"
+}
+
+# Deploy NIC - SRV 01
+resource "azurerm_network_interface" "vnic01" {
+  name                = "nic-vm-srv01"
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sub1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.PIP01.id
+  }
+}
+
+# Deploy NIC - SRV 02
+resource "azurerm_network_interface" "vnic02" {
+  name                = "nic-vm-srv02"
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sub2.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.PIP02.id
+  }
+}
+
+# Deploy VM - SRV 01
+resource "azurerm_windows_virtual_machine" "vm01" {
+  name                = "VM-SRV01-APP"
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+  size                = "Standard_B2S"
+  admin_username      = "adminuser"
+  admin_password      = "A1qa2ws3ed4rf5tg"
+  network_interface_ids = [
+    azurerm_network_interface.vnic01.id,
+  ]
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
+
+#Deploy VM - SRV 02
+resource "azurerm_windows_virtual_machine" "vm02" {
+  name                = "VM-SRV02-DB"
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+  size                = "Standard_B2S"
+  admin_username      = "adminuser"
+  admin_password      = "A1qa2ws3ed4rf5tg"
+  network_interface_ids = [
+    azurerm_network_interface.vnic02.id,
+  ]
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
+
+
---
+++ src/terraform/variables.tf
@@ -0,0 +1,25 @@
+#teste02#
+
+variable "locat" {
+  default = "eastus"
+}
+
+variable "RG" {
+  default = "rg-teste"
+}
+
+variable "vnet" {
+  default = "VNET-01"
+}
+
+variable "sub1" {
+  default = "SUB-LAN01"
+}
+
+variable "sub2" {
+  default = "SUB-LAN02"
+}
+
+variable "nsg-vnet" {
+  default = "NSG-default"
+}
